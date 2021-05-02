a39ce486212c092d395fcf6c824f5344390c616f
Saeid Gholami <saeidxgholami@gmail.com>
2021-05-02 19:45:35 +0430

    Create snake

--- snake/snake.py
+++ snake/snake.py
@@ -17,6 +17,23 @@ screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Snake")
 clock = pygame.time.Clock()
 
+class Snake(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH // 2
+        self.rect.centery = HEIGHT // 2
+        self.speedx = 1 
+
+    def update(self):
+        self.rect.x += self.speedx
+
+all_sprites = pygame.sprite.Group()
+snake = Snake()
+all_sprites.add(snake)
+
 running = True
 while running:
     clock.tick(FPS)
@@ -26,10 +43,12 @@ while running:
             running = False
 
     # update sprites
-
+    all_sprites.update()
     # draw / render
     screen.fill(BLACK)
     # draw sprites
+    all_sprites.draw(screen)
+
     pygame.display.flip()
 
 pygame.quit()
