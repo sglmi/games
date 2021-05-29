4880d82ef4ed0543b86d2d444cf20e40f6f7db14
Saeid Gholami <saeidxgholami@gmail.com>
2021-05-29 21:58:48 +0430

    api update save best scores

--- calc_fast/api.py
+++ calc_fast/api.py
@@ -3,60 +3,56 @@ import time
 
 # create random number
 def get_random_number(start=1, end=100):
-    """ Create random number between start and end range."""
+    """Create random number between start and end range."""
     return random.randint(start, end)
 
 
 # calculate operation
-def calculate(x, y, opr):
-    """ Calculate the result of x and y based on operator."""
-    if opr == "+":
-        return x + y
-    elif opr == "-":
-        return x - y
-    elif opr == "*":
-        return x * y
-    elif opr == "/":
-        try:
-            return x / y
-        except ZeroDivisionError:
-            pass
+def calculate(expresion):
+    """Return calculation result of expresion
+    If any exception occured return None."""
+    try:
+        return eval(expresion)
+    except Exception:
+        return None
+
 
 def compare(response, result):
-    if response == result:
-        return True
-    else:
-        return False
+    return response == result
 
-def add_point(point):
-    return point + 1
 
 # calculate current time
 def current_time():
     return time.time()
+
 
 # get time difference
 def get_time_diff(start_time, end_time):
     return end_time - start_time
 
 
+def get_socres():
+    scores = None
+    with open("scoreboard.txt", "r") as score_file:
+        scores = score_file.readlines()
+    scores = [score.strip() for score in scores]
+    return scores
+
+
+def save_score(scores):
+    with open("scoreboard.txt", "w") as score_file:
+        for score in scores:
+            score_file.write(f"{score}\n")
+
+
+def compare_scores(score, scores):
+    for item in scores[:]:
+        if score < float(item):
+            index = scores.index(item)
+            scores[index] = score
+            break
+    return scores  # modified scores
+
 
 if __name__ == "__main__":
-    point = 0
-    x = get_random_number()
-    y = get_random_number()
-
-    start_time = current_time()
-    response = int(input(f"{x} + {y} = "))
-    end_time = current_time()
-
-    answer = calculate(x, y, '+')
-    result = compare(response, answer)
-    time_diff = get_time_diff(start_time, end_time)
-    print(f'{time_diff:.2f}')
-
-    if result:
-        point += 1
-        print('correct')
-    else:
-        print('not correct')
+    pass
--- calc_fast/ui.py
+++ calc_fast/ui.py
@@ -1,16 +1,17 @@
 import os
 import api
 
-os.system('clear')
+os.system("clear")
+
 
 def menu():
     operator = input("Choice +, -, *, /: ")
-    score = 0
-    n = 3
-    start_range = 1
-    end_range = 10
+    correct_response = 0
+    n = counter = 3
+    start_range, end_range = (1, 10)
     total_response_time = 0
-    check_mark = "\U0001F603"
+    check_mark = "✅"
+    cross_mark = "❌"
     equations = []
 
     while n > 0:
@@ -18,32 +19,39 @@ def menu():
         number1 = api.get_random_number(start_range, end_range)
         number2 = api.get_random_number(start_range, end_range)
         equation = f"{number1} {operator} {number2}"
-        
-        # get response and set start and end time 
+
+        # get response and set start and end time
         start_time = api.current_time()
         response = int(input(f"{equation} = "))
         end_time = api.current_time()
 
-        # calculate response and total response time 
+        # calculate response and total response time
         response_time = api.get_time_diff(start_time, end_time)
         total_response_time += response_time
-        # get answer of a equation 
-        answer = api.calculate(number1, number2, operator)
+        # get answer of a equation
+        answer = api.calculate(equation)
         if api.compare(answer, response):
-            equation = f"{check_mark} {equation} = {answer}" 
-            score += 1
+            equation = f"{check_mark} {equation} = {answer}"
+            correct_response += 1
         else:
-            equation = f"X {equation} = {answer}"
+            equation = f"{cross_mark} {equation} = {answer}"
         equations.append(equation)
-        
+
         # clear everything and print each equation that formated
-        os.system('clear')
+        os.system("clear")
         for equation in equations:
             print(equation)
 
         n -= 1
-    print(f"Total Time {int(total_response_time)}")
+
+    print(f"Total Time {round(total_response_time, 2)}")
+    if correct_response == counter:
+        scores = api.get_socres()
+        score = round(total_response_time, 2)
+        scores = api.compare_scores(score, scores)
+        api.save_score(scores)
+        print("Score Saved!")
+
+
 if __name__ == "__main__":
     menu()
-
-        
